ecf487feea7d01459f25dc6c30e1947d322098e0
Boyue Pang <2096612213@qq.com>
2026-09-22 09:52:41 +0800

    更新首页

---
+++ sample/BookPage.jsx
@@ -0,0 +1,9 @@
+import Content from "./content.mdx";
+
+export default function BookPage() {
+  return (
+    <main className="book-page">
+      <Content />
+    </main>
+  );
+}
---
+++ sample/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import BookPage from "./BookPage.jsx";
+import "./style.css";
+
+import "katex/dist/katex.min.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <BookPage />
+);
